9cf7fadcccb162b0f643d1af612d2d36051f0c90
Nicolas Anzola <nicolasanzola200@gmail.com>
2023-06-19 15:19:19 -0500

    Starting

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from "@auth0/auth0-react"
+
+console.log(
+  process.env.REACT_APP_BASIC_LOGIN_DOMAIN
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Auth0Provider>
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
